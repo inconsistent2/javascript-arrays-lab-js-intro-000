90ddd0783931c0eceb0b2b8b9b5f2785ae7a05ff
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-13 09:08:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(kitten){
+  kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten){
+  kittens.unshift(kitten)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  }
